87b617a9d8d1fb7a2e31898c64da52c4309f90fd
ivan Rodriguez <ivanwork003@gmail.com>
2025-07-16 19:37:32 -0400

    Primer commit Django Befine Web

---
+++ clientes/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # rutas de clientes irán aquí luego
+]
---
+++ productos/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    # rutas de productos irán aquí luego
+]
